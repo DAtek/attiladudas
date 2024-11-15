e1cf7d8a810addf1d65a46f6018b72a875f0caff
Attila Dudas <dudasa7@gmail.com>
2024-11-15 17:42:42 +0100

    Update database testing tutorial

--- code_examples/db_testing/db/test_helpers.go
+++ code_examples/db_testing/db/test_helpers.go
@@ -1,21 +1,16 @@
 package db
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/DAtek/gotils"
 	"gorm.io/gorm"
 )
 
-var testConn *gorm.DB
-
-func GetTestConn() *gorm.DB {
-	if testConn == nil {
-		testConn = gotils.ResultOrPanic(NewConnFromEnv())
-	}
-
-	return testConn
-}
+var GetTestConn = sync.OnceValue(func() *gorm.DB {
+	return gotils.ResultOrPanic(NewConnFromEnv())
+})
 
 func GetTestTransaction() *gorm.DB {
 	return GetTestConn().Begin()
@@ -24,10 +19,9 @@ func GetTestTransaction() *gorm.DB {
 func TestWithTransaction(
 	transactionalTest func(t *testing.T, tx *gorm.DB),
 ) func(t *testing.T) {
-	tx := GetTestTransaction()
-
 	return func(t *testing.T) {
-		defer tx.Rollback()
+		tx := GetTestTransaction()
+		t.Cleanup(func() { tx.Rollback() })
 		transactionalTest(t, tx)
 	}
 }
